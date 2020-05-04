e1ce024fb2cbdc1224542eefbb81c3da0d7aa0ba
Mariani Giacomo <mariang1@t31300-lr138.org.aalto.fi>
2020-05-04 10:30:07 +0300

    new commits

---
+++ copy-player-bundle-to-server.sh
@@ -0,0 +1,1 @@
+cp jsav-exercise-player/build/jsav-exercise-player-bundle.js jsav-palyer-application-test-server/public/jsav-player/
---
+++ copy-recorder-bundle-to-server.sh
@@ -0,0 +1,1 @@
+cp jsav-exercise-recorder/build/jsav-exercise-recorder-bundle.js jsav-palyer-application-test-server/public/lib/
